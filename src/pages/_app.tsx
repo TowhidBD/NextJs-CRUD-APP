dd7ab3b9b6011bf27c43ce79d4e061d107226c9f
Towhid Hasan <towhid.hasan11@gmail.com>
2023-01-13 21:00:35 +0600

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import PrimeReact from 'primereact/api';
+
+import "primereact/resources/themes/mdc-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
+//font config
+import { Poppins } from '@next/font/google'
+
+const poppins = Poppins({
+    weight: ['400', '500', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+    variable: '--font-poppins'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    PrimeReact.ripple = true;
+
+    return (
+        <main className={`${poppins.variable} font-sans`}>
+            <Component {...pageProps} />
+        </main>
+    )
 }
